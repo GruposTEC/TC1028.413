6b0f185642b805028b1e6fc7a522844eeacc9a55
Antonio Mejia <avmejia@tec.mx>
2020-08-24 21:08:50 -0500

    200820
    200820

---
+++ Funciones/ejemplo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 09:32:36 2020
+
+@author: avmejia
+"""
+
+def mi_metodo(posicion):
+    """
+    Este es un metodo de prueba para enseñar docstring
+
+    Parameters
+    ----------
+    posicion : str
+        Mensaje a imprimir en el metodo.
+
+    Returns
+    -------
+    None.
+
+    """
+    print(posicion)
+    
+mi_metodo("Hola")
+
